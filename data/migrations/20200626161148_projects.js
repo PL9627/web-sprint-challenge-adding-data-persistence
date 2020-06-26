93d3e84cd37e720fb8ca448b6fffc434c7085cb0
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-06-26 16:16:10 -0700

    finished projects table in projects migration

--- data/migrations/20200626161148_projects.js
+++ data/migrations/20200626161148_projects.js
@@ -1,8 +1,12 @@
-
-exports.up = function(knex) {
-  
+exports.up = async function(knex) {
+  await knex.schema.createTable("projects", (table) => {
+      table.increments("Id")
+      table.text("name").notNull().unique()
+      table.text("description")
+      table.boolean("completed").notNull().defaultTo("false")
+  })
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists("projects")
 };
