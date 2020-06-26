3f5f17cae2372fefcd7627d46b3347bb885b42bd
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-06-26 16:31:27 -0700

    finished projects_resources table

--- data/migrations/20200626161148_projects.js
+++ data/migrations/20200626161148_projects.js
@@ -5,11 +5,13 @@ exports.up = async function (knex) {
     table.text("description");
     table.boolean("completed").notNull().defaultTo("false");
   });
+
   await knex.schema.createTable("resources", (table) => {
     table.increments("Id");
     table.text("name").notNull().unique();
     table.text("description");
   });
+
   await knex.schema.createTable("tasks", (table) => {
     table.increments("Id");
     table.text("name").notNull().unique();
@@ -23,10 +25,29 @@ exports.up = async function (knex) {
       .onUpdate("CASCADE");
     table.boolean("completed").notNull().defaultTo("false");
   });
+
+  await knex.schema.createTable("prjects_resources", (table) => {
+    table
+      .integer("project_ID")
+      .references("Id")
+      .inTable("projects")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+    table
+      .integer("resources_ID")
+      .references("Id")
+      .inTable("resources")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+  });
 };
 
 exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists("projects_resources");
+  
   await knex.schema.dropTableIfExists("tasks");
+  
   await knex.schema.dropTableIfExists("resources");
+  
   await knex.schema.dropTableIfExists("projects");
 };
