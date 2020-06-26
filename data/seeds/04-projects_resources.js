9c77c7b38983cfb02ef473a6079e9503502ff038
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-06-26 16:55:15 -0700

    code reformatting

--- data/seeds/04-projects_resources.js
+++ data/seeds/04-projects_resources.js
@@ -1,3 +1,3 @@
 exports.seed = async function (knex) {
-  await "projects_resources".insert([{ project_ID: 1, resources_ID: 1 }]);
+  await knex.("projects_resources").insert([{ project_ID: 1, resources_ID: 1 }]);
 };
