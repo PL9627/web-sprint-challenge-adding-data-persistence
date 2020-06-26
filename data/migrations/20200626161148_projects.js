f3eb538f07434027891cb5b764d4d686dc82dac9
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-06-26 16:19:47 -0700

    formatting and finished resources table

--- data/migrations/20200626161148_projects.js
+++ data/migrations/20200626161148_projects.js
@@ -1,12 +1,18 @@
-exports.up = async function(knex) {
+exports.up = async function (knex) {
   await knex.schema.createTable("projects", (table) => {
-      table.increments("Id")
-      table.text("name").notNull().unique()
-      table.text("description")
-      table.boolean("completed").notNull().defaultTo("false")
-  })
+    table.increments("Id");
+    table.text("name").notNull().unique();
+    table.text("description");
+    table.boolean("completed").notNull().defaultTo("false");
+  });
+  await knex.schema.createTable("resources", (table) => {
+    table.increments("Id");
+    table.text("name").notNull().unique();
+    table.text("description");
+  });
 };
 
-exports.down = async function(knex) {
-  await knex.schema.dropTableIfExists("projects")
+exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists("resources");
+  await knex.schema.dropTableIfExists("projects");
 };
