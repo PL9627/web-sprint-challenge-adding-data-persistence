44bc81ec2a89b40a4003c3c52d8052474a0b8558
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-06-26 17:00:03 -0700

    code reformatting

--- data/seeds/00-clean.js
+++ data/seeds/00-clean.js
@@ -1,6 +1,6 @@
 exports.seed = async function (knex) {
-  await knex("projects_resources").truncate();
-  await knex("tasks").truncate();
-  await knex("resources").truncate();
-  await knex("projects").truncate();
+  await knex("projects_resources").truncate()
+  await knex("tasks").truncate()
+  await knex("resources").truncate()
+  await knex("projects").truncate()
 };
